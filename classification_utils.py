465a5544383a149444cb37e645b67da1a990c63f
Sibbasson <sibbasson@hotmail.com>
2024-04-11 23:54:31 +0200

    Finally did scraping for entire second edition

--- classification_utils.py
+++ classification_utils.py
@@ -3,15 +3,18 @@ import numpy as np
 import pickle
 
 # Word frequencies 
-_frequencies = {}
-with open('word_frequencies.pickle', 'rb') as handle:
-    _frequencies = pickle.load(handle)
-_max_frequency = max(_frequencies.values())
+def get_word_frequencies() -> tuple[dict[str, int], int]:
+    _frequencies = {}
+    with open('word_frequencies.pickle', 'rb') as handle:
+        _frequencies = pickle.load(handle)
+    _max_frequency = max(_frequencies.values())
+    return _frequencies, _max_frequency
 
-def relative_word_frequency(word: str) -> float:
-    return _frequencies[word] / _max_frequency
 
-def line_to_datapoint(line: str):
+def relative_word_frequency(word: str, frequencies: dict[str, int], max_frequency: int) -> float:
+    return frequencies[word] / max_frequency
+
+def line_to_datapoint(line: str, frequencies: dict[str, int], max_frequency: int):
     return np.array([[
         ru.punctuation_after_first_word(line),
         ru.square_bracket(line),
@@ -19,6 +22,6 @@ def line_to_datapoint(line: str):
         ru.parentheses(line),
         ru.parentheses_with_punctuation(line),
         ru.category_word(line),
-        relative_word_frequency(ru.first_word_in_line(line).lower()),
+        relative_word_frequency(ru.words_in_line(line)[0].lower(), frequencies, max_frequency),
     ]])
 
