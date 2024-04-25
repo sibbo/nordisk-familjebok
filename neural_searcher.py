ac1a198ba1f5441e7ed019f340191857234140a2
Erik Ersmark <er5612er-s@student.lu.se>
2024-04-25 21:14:36 +0200

    Created matching data for manual annotation, and implemented code to test search-threshold.

--- neural_searcher.py
+++ neural_searcher.py
@@ -1,29 +1,30 @@
 from qdrant_client import QdrantClient
-#from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer
 
 class NeuralSearcher:
 
     def __init__(self, collection_name):
         self.collection_name = collection_name
         # Initialize encoder model
-        #self.model = SentenceTransformer('KBLab/sentence-bert-swedish-cased', device='cpu')
+        self.model = SentenceTransformer('KBLab/sentence-bert-swedish-cased', device='cpu')
         # initialize Qdrant client
         self.qdrant_client = QdrantClient(host='localhost', port=6333)
 
-    def search(self, vector):
+    def vector_search(self, vector, threshold: float):
         # Use `vector` for search for closest vectors in the collection
         search_result = self.qdrant_client.search(
             collection_name=self.collection_name,
             query_vector=vector,
             query_filter=None,  # We don't want any filters for now
-            limit=5  # 5 the most closest results is enough
+            limit=1,  # 1 most closest results is enough
+            score_threshold=threshold
         )
         # `search_result` contains found vector ids with similarity scores along with the stored payload
         # In this function we are interested in payload only
         payloads = [hit.payload for hit in search_result]
         return payloads
 
-    # def search(self, text: str):
-    #     # Convert text query into vector
-    #     vector = self.model.encode(text).tolist()
-    #     return search(vector)
+    def string_search(self, text: str, threshold: float):
+        # Convert text query into vector
+        vector = self.model.encode(text).tolist() 
+        return self.vector_search(vector, threshold=threshold) # This does not work
